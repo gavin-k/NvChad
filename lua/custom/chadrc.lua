2f0d83a564bb7f67e6f2e0e45c9325c41f291358
gavin-k <lijy0717@gmail.com>
2022-10-27 18:28:54 +0800

    use new plugin syntax style

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,9 +1,9 @@
 -- Just an example, supposed to be placed in /lua/custom/
 -- local env = require "custom.env"
-local present, env = pcall(require, "custom.env")
-if not present then
-  env = require "custom.env-example"
-end
+-- local present, env = pcall(require, "custom.env")
+-- if not present then
+--   env = require "custom.env-example"
+-- end
 local M = {}
 
 -- make sure you maintain the structure of `core/default_config.lua` here,
@@ -19,93 +19,96 @@ M.ui = {
   hl_override = require "custom.highlights",
 }
 
-M.plugins = {
-  user = require "custom.plugins",
-  -- https://nvchad.com/config/plugins#override-default-config-of-a-plugin
-  -- override = {
-  --   ["folke/which-key.nvim"] = {
-  --     disable = false,
-  --   },
-  -- },
-  override = {
-    ["nvim-telescope/telescope.nvim"] = {
-      extensions = {
-        -- configure for project ext
-        project = {
-          base_dirs = env.project_base_dirs,
-          hidden_files = true, -- default: false
-          theme = "dropdown",
-        },
-      },
-      -- this will override the default config. can't use theme etc.
-      -- extensions_list = { "project" } ,
-    },
-    ["goolord/alpha-nvim"] = {
-      header = {
-        type = "text",
-        val = {
-          "██████   ██████      ██ ████████     ███    ██  ██████  ██     ██ ",
-          "██   ██ ██    ██     ██    ██        ████   ██ ██    ██ ██     ██ ",
-          "██   ██ ██    ██     ██    ██        ██ ██  ██ ██    ██ ██  █  ██ ",
-          "██   ██ ██    ██     ██    ██        ██  ██ ██ ██    ██ ██ ███ ██ ",
-          "██████   ██████      ██    ██        ██   ████  ██████   ███ ███  ",
-          "                                                                  ",
-          "                                                                  ",
-        },
-        opts = {
-          position = "center",
-          hl = "AlphaHeader",
-        },
-      },
-      buttons = {
-        val = {
-          -- button icon; https://www.nerdfonts.com/cheat-sheet
-          require("custom.helper").button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
-          -- require'custom.helper'.button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
-          require("custom.helper").button("SPC f o", "  Recent File  ", ":Telescope oldfiles<CR>"),
-          require("custom.helper").button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
-          require("custom.helper").button("SPC b m", "  Bookmarks  ", ":Telescope marks<CR>"),
-          require("custom.helper").button("SPC t h", "  Themes  ", ":Telescope themes<CR>"),
-          require("custom.helper").button("SPC p p", "  Projects  ", ":Telescope project<CR>"),
-          require("custom.helper").button("SPC e s", "  Settings", ":e $MYVIMRC | :cd %:p:h <CR>"),
-        },
-      },
-    },
+M.plugins = require "custom.plugins"
 
-    -- Override plugin config
-    ["williamboman/mason.nvim"] = {
-      override_options = {
-        ensure_installed = {
-          "lua-language-server",
-          "html-lsp",
-          "intelephense",
-          "json-lsp",
-          "php-cs-fixer",
-          "phpcs",
-          "prettier",
-          "shellcheck",
-          "stylua", -- format lua code
-          "clangd",
-        },
-      },
-    },
-
-    ["nvim-treesitter/nvim-treesitter"] = {
-      override_options = {
-        ensure_installed = {
-          "lua",
-          "php",
-          "html",
-          "css",
-          "go",
-          "java",
-          "javascript",
-          "rust",
-        },
-      },
-    },
-  },
-}
+-- old plugin style
+-- M.plugins = {
+--   user = require "custom.plugins",
+--   -- https://nvchad.com/config/plugins#override-default-config-of-a-plugin
+--   -- override = {
+--   --   ["folke/which-key.nvim"] = {
+--   --     disable = false,
+--   --   },
+--   -- },
+--   override = {
+--     ["nvim-telescope/telescope.nvim"] = {
+--       extensions = {
+--         -- configure for project ext
+--         project = {
+--           base_dirs = env.project_base_dirs,
+--           hidden_files = true, -- default: false
+--           theme = "dropdown",
+--         },
+--       },
+--       -- this will override the default config. can't use theme etc.
+--       -- extensions_list = { "project" } ,
+--     },
+--     ["goolord/alpha-nvim"] = {
+--       header = {
+--         type = "text",
+--         val = {
+--           "██████   ██████      ██ ████████     ███    ██  ██████  ██     ██ ",
+--           "██   ██ ██    ██     ██    ██        ████   ██ ██    ██ ██     ██ ",
+--           "██   ██ ██    ██     ██    ██        ██ ██  ██ ██    ██ ██  █  ██ ",
+--           "██   ██ ██    ██     ██    ██        ██  ██ ██ ██    ██ ██ ███ ██ ",
+--           "██████   ██████      ██    ██        ██   ████  ██████   ███ ███  ",
+--           "                                                                  ",
+--           "                                                                  ",
+--         },
+--         opts = {
+--           position = "center",
+--           hl = "AlphaHeader",
+--         },
+--       },
+--       buttons = {
+--         val = {
+--           -- button icon; https://www.nerdfonts.com/cheat-sheet
+--           require("custom.helper").button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
+--           -- require'custom.helper'.button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
+--           require("custom.helper").button("SPC f o", "  Recent File  ", ":Telescope oldfiles<CR>"),
+--           require("custom.helper").button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
+--           require("custom.helper").button("SPC b m", "  Bookmarks  ", ":Telescope marks<CR>"),
+--           require("custom.helper").button("SPC t h", "  Themes  ", ":Telescope themes<CR>"),
+--           require("custom.helper").button("SPC p p", "  Projects  ", ":Telescope project<CR>"),
+--           require("custom.helper").button("SPC e s", "  Settings", ":e $MYVIMRC | :cd %:p:h <CR>"),
+--         },
+--       },
+--     },
+--
+--     -- Override plugin config
+--     ["williamboman/mason.nvim"] = {
+--       override_options = {
+--         ensure_installed = {
+--           "lua-language-server",
+--           "html-lsp",
+--           "intelephense",
+--           "json-lsp",
+--           "php-cs-fixer",
+--           "phpcs",
+--           "prettier",
+--           "shellcheck",
+--           "stylua", -- format lua code
+--           "clangd",
+--         },
+--       },
+--     },
+--
+--     ["nvim-treesitter/nvim-treesitter"] = {
+--       override_options = {
+--         ensure_installed = {
+--           "lua",
+--           "php",
+--           "html",
+--           "css",
+--           "go",
+--           "java",
+--           "javascript",
+--           "rust",
+--         },
+--       },
+--     },
+--   },
+-- }
 
 -- nvim mappings shortcuts
 M.mappings = require "custom.mappings"
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,96 @@
 -- https://nvchad.github.io/config/plugins
+local present, env = pcall(require, "custom.env")
+if not present then
+  env = require "custom.env-example"
+end
 
 return {
+
+  -- system plugin override
+  ["nvim-telescope/telescope.nvim"] = {
+    config = function()
+      require "plugins.configs.telescope"
+      require "custom.plugins.telescope"
+    end,
+    extensions = {
+      -- configure for project ext
+      project = {
+        base_dirs = env.project_base_dirs,
+        hidden_files = true, -- default: false
+        theme = "dropdown",
+      },
+    },
+    -- this will override the default config. can't use theme etc.
+    -- extensions_list = { "project" } ,
+  },
+  ["goolord/alpha-nvim"] = {
+    disable = false,
+    override_options = {
+      header = {
+        type = "text",
+        val = {
+          "██████   ██████      ██ ████████     ███    ██  ██████  ██     ██ ",
+          "██   ██ ██    ██     ██    ██        ████   ██ ██    ██ ██     ██ ",
+          "██   ██ ██    ██     ██    ██        ██ ██  ██ ██    ██ ██  █  ██ ",
+          "██   ██ ██    ██     ██    ██        ██  ██ ██ ██    ██ ██ ███ ██ ",
+          "██████   ██████      ██    ██        ██   ████  ██████   ███ ███  ",
+          "                                                                  ",
+          "                                                                  ",
+        },
+        opts = {
+          position = "center",
+          hl = "AlphaHeader",
+        },
+      },
+      buttons = {
+        val = {
+          -- button icon; https://www.nerdfonts.com/cheat-sheet
+          require("custom.helper").button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
+          -- require'custom.helper'.button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
+          require("custom.helper").button("SPC f o", "  Recent File  ", ":Telescope oldfiles<CR>"),
+          require("custom.helper").button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
+          require("custom.helper").button("SPC b m", "  Bookmarks  ", ":Telescope marks<CR>"),
+          require("custom.helper").button("SPC t h", "  Themes  ", ":Telescope themes<CR>"),
+          require("custom.helper").button("SPC p p", "  Projects  ", ":Telescope project<CR>"),
+          require("custom.helper").button("SPC e s", "  Settings", ":e $MYVIMRC | :cd %:p:h <CR>"),
+        },
+      },
+    },
+  },
+
+  -- Override plugin config
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "intelephense",
+        "json-lsp",
+        "php-cs-fixer",
+        "phpcs",
+        "prettier",
+        "shellcheck",
+        "stylua", -- format lua code
+        "clangd",
+      },
+    },
+  },
+
+  ["nvim-treesitter/nvim-treesitter"] = {
+    override_options = {
+      ensure_installed = {
+        "lua",
+        "php",
+        "html",
+        "css",
+        "go",
+        "java",
+        "javascript",
+        "rust",
+      },
+    },
+  },
+
   ["phaazon/hop.nvim"] = {
     branch = "v2", -- optional but strongly recommended
     config = function()
@@ -44,9 +134,10 @@ return {
   ["folke/which-key.nvim"] = {
     disable = false,
   },
-  ["goolord/alpha-nvim"] = {
-    disable = false,
-  },
+  -- ["goolord/alpha-nvim"] = {
+  --
+  --   disable = false,
+  -- },
 
   -- https://github.com/nvim-telescope/telescope-project.nvim#workspace-mappings-insert-mode
   ["nvim-telescope/telescope-project.nvim"] = {
@@ -56,12 +147,12 @@ return {
       -- require'telescope'.load_extension('project')
     end,
   },
-  ["nvim-telescope/telescope.nvim"] = {
-    config = function()
-      require "plugins.configs.telescope"
-      require "custom.plugins.telescope"
-    end,
-  },
+  -- ["nvim-telescope/telescope.nvim"] = {
+  --   config = function()
+  --     require "plugins.configs.telescope"
+  --     require "custom.plugins.telescope"
+  --   end,
+  -- },
 
   -- support DAP. clients: https://microsoft.github.io/debug-adapter-protocol/implementors/adapters/
   ["mfussenegger/nvim-dap"] = {
